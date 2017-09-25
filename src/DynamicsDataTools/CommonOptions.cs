70fcd7ae0df3374d9c993a5d3785bde90cdff9a1
Cristhian Fernandez <crisfervil@gmail.com>
2017-09-25 19:00:49 +0100

    Added import options

--- src/DynamicsDataTools/CommonOptions.cs
+++ src/DynamicsDataTools/CommonOptions.cs
@@ -4,7 +4,7 @@ namespace DynamicsDataTools
 {
     public class CommonOptions
     {
-        [Option("connection", Required = true)]
+        [Option("connection", Required = true, HelpText ="Connection string, or name of a connection string to use")]
         public string ConnectionName { get; set; }
 
         [Option("debug-brk", HelpText = "Launches the debugger before running the selected command")]
--- src/DynamicsDataTools/Tools/ImportToolOptions.cs
+++ src/DynamicsDataTools/Tools/ImportToolOptions.cs
@@ -2,10 +2,10 @@
 
 namespace DynamicsDataTools.Tools
 {
+    [Verb("import", HelpText = "Imports information from a file to Dynamics")]
     public class ImportToolOptions : CommonOptions
     {
         [Option("file", HelpText = "File containing the data to import")]
         public string File { get; set; }
-
     }
 }
