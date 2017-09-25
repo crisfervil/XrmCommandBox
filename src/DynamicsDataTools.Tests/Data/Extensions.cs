3229597414b3cac615c11acd8db047608a4e77e2
Cristhian Fernandez <crisfervil@gmail.com>
2017-09-25 19:18:30 +0100

    Added test for data table converted. TBC

---
+++ src/DynamicsDataTools.Tests/Data/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DynamicsDataTools.Tests.Data
+{
+    [TestClass]
+    public class Extensions
+    {
+        [TestMethod]
+        public void Converts_To_Data_Table()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
