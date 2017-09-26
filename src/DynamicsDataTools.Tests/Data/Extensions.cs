48074c5109bc744e79c02e2d657d1b4dfdfe9ee5
Cristhian Fernandez <crisfervil@gmail.com>
2017-09-26 18:09:20 +0100

    Updated tests

--- src/DynamicsDataTools.Tests/Data/Extensions.cs
+++ src/DynamicsDataTools.Tests/Data/Extensions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using DynamicsDataTools.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xrm.Sdk;
 using System;
 
 namespace DynamicsDataTools.Tests.Data
@@ -9,7 +11,28 @@ namespace DynamicsDataTools.Tests.Data
         [TestMethod]
         public void Converts_To_Data_Table()
         {
-            throw new NotImplementedException();
+            var data = new EntityCollection();
+
+            var referenceGuid1 = Guid.NewGuid();
+            var entity1 = new Entity("myEntity1");
+            entity1["attr1"] = "Value1";
+            entity1["attr2"] = new EntityReference("myEntity2", referenceGuid1);
+            data.Entities.Add(entity1);
+
+            var entity2 = new Entity("myEntity1");
+            entity2["attr1"] = new OptionSetValue(12);
+            entity2["attr2"] = new Money(16.55m);
+            data.Entities.Add(entity2);
+
+            var dt = data.AsDataTable();
+
+            // check output values
+            Assert.AreEqual("Value1", dt[0]["attr1"]);
+            Assert.AreEqual("myEntity2", ((EntityReferenceValue)dt[0]["attr2"]).LogicalName);
+            Assert.AreEqual(referenceGuid1, ((EntityReferenceValue)dt[0]["attr2"]).Value);
+
+            Assert.AreEqual(12, dt[1]["attr1"]);
+            Assert.AreEqual(16.55m, dt[1]["attr2"]);
         }
     }
 }
