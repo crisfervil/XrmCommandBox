0f9079ab79c19c0e7877e48d49d14f406eff03b1
Cristhian Fernandez <crisfervil@gmail.com>
2017-10-09 10:53:48 +0100

    Added solution export command

---
+++ src/XrmCommandBox/Tools/SolutionExportTool.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+
+namespace XrmCommandBox.Tools
+{
+    public class SolutionExportTool
+    {
+        private readonly ILog _log = LogManager.GetLogger(typeof(SolutionExportTool));
+        private readonly IOrganizationService _crmService;
+
+        public SolutionExportTool(IOrganizationService service)
+        {
+            _crmService = service;
+        }
+
+        public void Run(SolutionExportToolOptions options)
+        {
+            _log.Info("Running Solution Export tool...");
+
+            _log.Debug($"Solution Name: {options.SolutionName}");
+
+            var fileName = $"{options.SolutionName}.zip";
+            _log.Debug($"File Name: {fileName}");
+
+            var request = new ExportSolutionRequest
+            {
+                // TODO: Add more options
+                SolutionName = options.SolutionName
+            };
+
+            var response = (ExportSolutionResponse)_crmService.Execute(request);
+
+            // TODO: Create log file
+
+            File.WriteAllBytes(fileName, response.ExportSolutionFile);
+
+            _log.Info("done");
+        }
+    }
+}
---
+++ src/XrmCommandBox/Tools/SolutionExportToolOptions.cs
@@ -0,0 +1,11 @@
+﻿using CommandLine;
+
+namespace XrmCommandBox.Tools
+{
+    [Verb("solution-export", HelpText = "Export the specified solution to a file")]
+    public class SolutionExportToolOptions
+    {
+        [Option("solution-name", HelpText = "Unique name of the solution to export")]
+        public string SolutionName { get; set; }
+    }
+}
