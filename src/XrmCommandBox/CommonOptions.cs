5c7933f6f8dcc00a5df90a280d846349347c300f
Cristhian Fernandez <crisfervil@gmail.com>
2017-10-10 16:52:17 +0100

    Added Common Options Short Names

--- src/XrmCommandBox/CommonOptions.cs
+++ src/XrmCommandBox/CommonOptions.cs
@@ -4,19 +4,19 @@ namespace XrmCommandBox
 {
     public class CommonOptions
     {
-        [Option("connection", Required = true, HelpText ="Connection string, or name of a connection string to use")]
+        [Option('c', "connection", Required = true, HelpText ="Connection string, or name of a connection string to use")]
         public string ConnectionName { get; set; }
 
-        [Option("debug-brk", HelpText = "Launches the debugger before running the selected command")]
+        [Option('b', "debug-brk", HelpText = "Launches the debugger before running the selected command")]
         public bool DebugBreak { get; set; }
 
-        [Option("config-file", HelpText = "Xml file containing the command options")]
+        [Option('f', "config-file", HelpText = "Xml file containing the command options")]
         public string ConfigFile { get; set; }
 
-        [Option("continue-on-error", HelpText = "Continue if there's an error while processing the command")]
+        [Option('e', "continue-on-error", HelpText = "Continue if there's an error while processing the command")]
         public bool ContinueOnError { get; set; }
 
-        [Option("log-level", HelpText = "Sets the current logging output. Can be Debug, Info, Error")]
+        [Option('l', "log-level", HelpText = "Sets the current logging output. Can be Debug, Info, Error")]
         public LogLevels LogLevel { get; set; } = LogLevels.Info;
     }
 }
