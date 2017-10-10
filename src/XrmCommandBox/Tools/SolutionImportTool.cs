34bfebbc736753d21db4793229f0a9619d9cc813
Cristhian Fernandez <crisfervil@gmail.com>
2017-10-10 19:04:07 +0100

    Added solution import command

---
+++ src/XrmCommandBox/Tools/SolutionImportTool.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using log4net;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using System.Threading;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace XrmCommandBox.Tools
+{
+    public class SolutionImportTool
+    {
+        private readonly IOrganizationService _crmService;
+        private readonly ILog _log = LogManager.GetLogger(typeof(SolutionExportTool));
+
+        public SolutionImportTool(IOrganizationService service)
+        {
+            _crmService = service;
+        }
+
+        public void Run(SolutionImportToolOptions options)
+        {
+            _log.Info("Running Solution Import Tool...");
+
+            _log.Debug($"Solution File: {options.SolutionFile}");
+
+            _log.Info($"Reading file {options.SolutionFile}...");
+
+            var fileContents = File.ReadAllBytes(options.SolutionFile);
+
+            _log.Info($"{fileContents.Length} bytes read");
+
+            var importJobId = Guid.NewGuid();
+            var request = new ImportSolutionRequest
+            {
+                // TODO: Add more options
+                CustomizationFile = fileContents,
+                ImportJobId = importJobId
+            };
+
+
+            if (options.Async)
+            {
+                _log.Info("Start importing asynchronously...");
+                var asyncRequest = new ExecuteAsyncRequest
+                {
+                    Request = request
+                };
+
+                var response = (ExecuteAsyncResponse) _crmService.Execute(asyncRequest);
+                AwaitCompletion(response.AsyncJobId);
+            }
+            else
+            {
+                _log.Info("Importing synchrounously...");
+                var response = (ImportSolutionResponse) _crmService.Execute(request);
+                // TODO: Check the response
+            }
+
+            _log.Info("Done!");
+        }
+
+        private void AwaitCompletion(Guid asyncJobId)
+        {
+            // TODO: Set the timeout as a parameter option
+            var asyncTimeout = 3600; // seconds
+            var sleepInterval = 1; // secconds
+            var end = DateTime.Now.AddSeconds(asyncTimeout);
+
+            var completed = false;
+            while (!completed)
+            {
+                if (end < DateTime.Now)
+                {
+                    throw new Exception($"Import Timeout Exceeded: {asyncTimeout}");
+                }
+
+                Thread.Sleep(sleepInterval * 1000);
+                _log.Debug($"Sleeping for {sleepInterval} seconds");
+
+                Entity asyncOperation;
+
+                try
+                {
+                    asyncOperation = _crmService.Retrieve("asyncoperation", asyncJobId, new ColumnSet("asyncoperationid", "statuscode", "message"));
+                }
+                catch (Exception ex)
+                {
+                    _log.Debug(ex);
+                    _log.Warn(ex.Message);
+                    continue;
+                }
+
+                var statusCode = (OptionSetValue)asyncOperation["statuscode"];
+                _log.Debug($"Status Code: {statusCode.Value}");
+
+                switch (statusCode.Value)
+                {
+                    //Succeeded
+                    case 30:
+                        completed = true;
+                        break;
+                    //Pausing //Canceling //Failed //Canceled
+                    case 21:
+                    case 22:
+                    case 31:
+                    case 32:
+                        throw new Exception($"Solution Import Failed: {statusCode.Value} {asyncOperation["message"]}");
+                }
+            }
+
+        }
+    }
+}
---
+++ src/XrmCommandBox/Tools/SolutionImportToolOptions.cs
@@ -0,0 +1,15 @@
+﻿using CommandLine;
+
+namespace XrmCommandBox.Tools
+{
+    [Verb("solution-import", HelpText = "Imports the specified solution .zip file into CRM")]
+    [Handler(typeof(SolutionImportTool))]
+    public class SolutionImportToolOptions : CommonOptions
+    {
+        [Option('s', "solution-file", Required = true, HelpText = ".zip file containing the solution to import")]
+        public string SolutionFile { get; set; }
+
+        [Option('a', "async", HelpText = "Indicates wether the import should be performed asynchronously")]
+        public bool Async { get; set; }
+    }
+}
