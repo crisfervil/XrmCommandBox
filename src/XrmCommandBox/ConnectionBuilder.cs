30f4ad6871460e0a2b708a217dc770fcf3c9b836
Cristhian Fernandez <crisfervil@gmail.com>
2018-08-25 19:40:03 +0100

    Added connection from connections.config file

--- src/XrmCommandBox/ConnectionBuilder.cs
+++ src/XrmCommandBox/ConnectionBuilder.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Configuration;
+using System.Xml.Linq;
+using System.Linq;
 using log4net;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Tooling.Connector;
@@ -12,15 +14,43 @@ namespace XrmCommandBox
 
         public IOrganizationService GetConnection(string connection)
         {
+            if (connection == null) throw new ArgumentException("connection parameter missing");
+
             _log.Info("Connecting to CRM...");
 
-            // The connection can be a connection name in the app.config file or a connection string
-            var connStr = ConfigurationManager.ConnectionStrings[connection];
-            var connStrValue = connection;
-            if (connStr != null)
+            ConnectionStringSettings connStrSetting = null;
+            string connStrValue = null;
+
+            // Try to get the connection string from a connections.config file in the current path
+            var connectionsPath = "connections.config";
+            connectionsPath = System.IO.Path.Combine(System.Environment.CurrentDirectory, connectionsPath);
+            if (System.IO.File.Exists(connectionsPath))
             {
-                _log.Debug($"Using ConnectionString {connection}");
-                connStrValue = connStr.ConnectionString;
+                // the connections.config file exists, so try to read the connectionstring from there
+                _log.Debug($"Trying to get the connection from current dir connections.config");
+
+                var connectionsStringDoc = XDocument.Load(connectionsPath);
+                var connsQry = from c in connectionsStringDoc.Descendants("add")
+                               where c.Attribute("name")?.Value == connection
+                            select c.Attribute("connectionString")?.Value;
+                connStrValue = connsQry.FirstOrDefault();
+            }
+
+            if (connStrValue == null)
+            {
+                _log.Debug($"Trying to get the connection from XrmCommandBox AppSettings...");
+                connStrSetting = ConfigurationManager.ConnectionStrings[connection];
+                if (connStrSetting != null)
+                {
+                    _log.Debug($"{connection} found");
+                    connStrValue = connStrSetting.ConnectionString;
+                }
+            }
+
+            // connection not found in conf files. Assume the full conn string was passed
+            if (connStrValue == null)
+            {
+                connStrValue = connection;
             }
 
             var client = new CrmServiceClient(connStrValue);
